d95b5753d41b4743855f83dc8919c14ea380e9b1
fisherbyu <fisherandrew777@gmail.com>
2023-03-09 18:41:34 -0700

    updates

--- BookStore/Infrastructure/UrlExtensions.cs
+++ BookStore/Infrastructure/UrlExtensions.cs
@@ -8,7 +8,10 @@ namespace BookStore.Infrastructure
 {
     public static class UrlExtensions
     {
+        //public static string PathAndQuery(this HttpRequest request) =>
+        //    request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.PathAndQuery().ToString();
+
         public static string PathAndQuery(this HttpRequest request) =>
-            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.PathAndQuery().ToString();
+            request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
     }
 }
